2299dcca84b486847ba2d6fb47bb5b10a11608af
Arkadiusz Stankiewicz <arek.kolo978@gmail.com>
2023-07-25 12:47:47 +0200

    added Fibo

--- main.py
+++ main.py
@@ -2,4 +2,11 @@
 def range_generator(n):
     return list(range(n))
 
+def fib_generator(n):
+    if n < 2:   
+        return n
+    else:
+        return n * fib_generator(n - 1)
+
 print(range_generator(5))
+print(fib_generator(5))
